fbf4ee5fef540f6e0ff3747c6ac727fbc8f6fbe2
boyofdoom09-maker <boyofdoom09@outlook.com>
2025-09-04 12:49:47 -0500

    Update program_1.py

--- program_1.py
+++ program_1.py
@@ -2,5 +2,7 @@ def hello_world():
     # Update the message below to say "Hello World", 
     # Python cares about white space, so watch your indentation.
     print('Hello World')
-
+# Elijah Quigley
+# date:9/4/2025
+# Hello World Project
 hello_world()
